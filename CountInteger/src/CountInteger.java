e3755101190592c59d4197ca9d8c46e1746dc071
theRealScoobaSteve <stephensimone@live.com>
2018-02-10 19:38:10 -0800

    Finished CountInteger

--- CountInteger/src/CountInteger.java
+++ CountInteger/src/CountInteger.java
@@ -1,41 +1,37 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class CountInteger {
+    static Scanner input = new Scanner(System.in);
+    static int positive, negative;
+    static int total;
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
         System.out.print("Enter integers, input ends with 0: ");
-        String input = in.nextLine();
-        String values[] = input.split("\\s");
-
-        if( values[0].equals("0") ) {
-            System.out.print("No numbers were entered except 0\n");
-        }
-        else {
-            int length = values.length;
-            int posCount = 0;
-            int negCount = 0;
-            int total = 0;
-            int totalCount = 0;
-            for( int i = 0; i < length; ++i) {
-                int value = Integer.parseInt( values[i] );
-                total += value;
-
-                if( value < 0)
-                    negCount++;
-                else
-                    posCount++;
-
-                totalCount++;
+        int index = 0;
+        do {
+            int here = input.nextInt();
+            if (here != 0) {
+                total += here;
+                index++;
+                if (here < 0) {
+                    negative++;
+                } else {
+                    positive++;
+                }
+            } else {
+                break;
             }
-
-            System.out.println(total);
-            System.out.println(totalCount);
-            double average = (double)(total / totalCount);
-            System.out.println(average);
-            System.out.println("The number of positives is " + posCount);
-            System.out.println("The number of negatives is " + negCount);
-            System.out.println("The total count is " + totalCount);
-            System.out.println("The average is " + average);
+        } while (true);
+        if (total != 0) {
+            System.out.println("The number of positives is " + positive);
+            System.out.println("The number of negatives is " + negative);
+            System.out.println("The total count is " + index);
+            System.out.printf("The average is %.2f\n", (double) total / index);
+        } else {
+            System.out.println("No numbers were entered except 0");
         }
     }
 }
+
