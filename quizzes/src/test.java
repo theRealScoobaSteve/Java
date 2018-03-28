1bc0b0d41b9d62bf012735e0579a1a2757e38cd6
theRealScoobaSteve <stephensimone@live.com>
2018-03-27 17:53:05 -0700

    Added whats new

--- quizzes/src/test.java
+++ quizzes/src/test.java
@@ -1,11 +1,11 @@
 public class test {
     public static void main(String[] args) {
-        int i = 1;
-        int j = 1;
-        while( i < 5) {
-            i++;
-            j = j *2;
-        }
-        System.out.println(j);
+        int[] x = {0, 1, 2, 3, 4, 5};
+        xMethod(x, 5);
+    }
+
+    public static void xMethod(int[] x, int length) {
+        for (int i = 0; i < length; i++)
+            System.out.print(" " + x[i]);
     }
 }
